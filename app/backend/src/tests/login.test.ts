a73ba140e3ff448a6dbf0f3ca22ee73e466d3768
Ckauark <ckauark@gmail.com>
2022-10-26 16:53:15 -0300

    feat: test-coverage 70%

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -17,14 +17,14 @@ describe('Testa a rota login', () => {
 
   let chaiHttpResponse: Response;
 
-  before(async () => {
+  beforeEach(async () => {
     sinon
       .stub(User, "findOne")
       .resolves(userMock as unknown as User);
     sinon.stub(jwt, 'sign').resolves(tokenMock);
   });
 
-  after(()=>{
+  afterEach(()=>{
     (User.findOne as sinon.SinonStub).restore();
     (jwt.sign as sinon.SinonStub).restore();
   })
@@ -86,33 +86,35 @@ describe('Testa a rota login', () => {
       message: 'Incorrect email or password',
     });
   });
+});
+
+describe('Testa a rota login/validate', () => {
+
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(User, "findByPk")
+      .resolves(userMock as unknown as User);
+  });
+
+  afterEach(()=>{
+    (User.findByPk as sinon.SinonStub).restore();
+    (jwt.verify as sinon.SinonStub).restore();
+  })
 
 
-  // it('testa se o acesso é permitido com o token valido', async () => {
-  //   const chaiHttpResponse = await chai
-  //      .request(app)
-  //      .get('/login/validate')
-  //      .set('Authorization', tokenMock);
-
-  //   expect(chaiHttpResponse.status).to.equal(200);
-  //   expect(chaiHttpResponse.body).to.deep.equal({
-  //     role: 'user',
-  //   });
-  // });
-
-  // it('testa se o acesso não é permitido com o token invalido', async () => {
-  //   (jwt.sign as sinon.SinonStub).restore();
-  //   sinon.stub(jwt, 'sign').resolves("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.");
-
-  //   const chaiHttpResponse = await chai
-  //      .request(app)
-  //      .get('/login/validate')
-  //      .set('Authorization', tokenMock);
-
-  //   expect(chaiHttpResponse.status).to.equal(200);
-  //   expect(chaiHttpResponse.body).to.deep.equal({
-  //     role: 'user',
-  //   });
-  // });
-
+  it('testa se o acesso é permitido com o token valido', async () => {
+    sinon.stub(jwt, 'verify').resolves(userMock);
+    const chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('Authorization', tokenMock);
+  
+    expect(chaiHttpResponse.status).to.equal(200);
+    expect(chaiHttpResponse.body).to.deep.equal({
+      role: 'user',
+    });
+  });
+  
 });
