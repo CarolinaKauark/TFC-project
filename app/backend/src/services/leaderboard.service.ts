d1e55f81dfdb45ccc5640993194416785f5b001a
Ckauark <ckauark@gmail.com>
2022-11-03 11:23:08 -0300

    cleaning comments

--- app/backend/src/services/leaderboard.service.ts
+++ app/backend/src/services/leaderboard.service.ts
@@ -34,69 +34,6 @@ SUM(totalDraws) AS totalDraws,
 SUM(totalGames) AS totalGames
 FROM ((${queryHome}) UNION (${queryAway})) AS newBoard GROUP BY name`;
 
-// `SELECT
-// name,
-// SUM(goalsFavor) AS goalsFavor,
-// SUM(goalsOwn) AS goalsOwn,
-// SUM(goalsBalance) goalsBalance,
-// SUM(totalVictories) AS totalVictories,
-// SUM(totalLosses) AS totalLosses,
-// SUM(totalDraws) AS totalDraws,
-// SUM(totalGames) AS totalGames
-// FROM (
-// ()
-// UNION
-// ()) AS leaderboard
-// GROUP BY name`
-
-// const testQuery = `SELECT *,
-// (totalVictories * 3 + totalDraws) AS totalPoints,
-// ROUND(((totalVictories * 3 + totalDraws) / (totalGames * 3) * 100), 2) AS efficiency
-// FROM (
-// SELECT
-// name,
-// SUM(goalsFavor) AS goalsFavor,
-// SUM(goalsOwn) AS goalsOwn,
-// SUM(goalsBalance) goalsBalance,
-// SUM(totalVictories) AS totalVictories,
-// SUM(totalLosses) AS totalLosses,
-// SUM(totalDraws) AS totalDraws,
-// SUM(totalGames) AS totalGames
-// FROM (
-// (SELECT
-// te.team_name as name,
-// SUM(home_team_goals) as goalsFavor,
-// SUM(away_team_goals) AS goalsOwn,
-// SUM(home_team_goals - away_team_goals) AS goalsBalance,
-// SUM(home_team_goals > away_team_goals) AS totalVictories,
-// SUM(home_team_goals < away_team_goals) AS totalLosses,
-// SUM(home_team_goals = away_team_goals) AS totalDraws,
-// COUNT(*) AS totalGames
-// FROM TRYBE_FUTEBOL_CLUBE.matches as ma
-// INNER JOIN TRYBE_FUTEBOL_CLUBE.teams as te
-// ON te.id = ma.home_team
-// where in_progress = 0
-// GROUP BY te.team_name)
-// UNION
-// (SELECT
-// te.team_name as name,
-// SUM(away_team_goals) as goalsFavor,
-// SUM(home_team_goals) AS goalsOwn,
-// SUM(away_team_goals - home_team_goals) AS goalsBalance,
-// SUM(away_team_goals > home_team_goals) AS totalVictories,
-// SUM(away_team_goals < home_team_goals) AS totalLosses,
-// SUM(away_team_goals = home_team_goals) AS totalDraws,
-// COUNT(*) AS totalGames
-// FROM TRYBE_FUTEBOL_CLUBE.matches as ma
-// INNER JOIN TRYBE_FUTEBOL_CLUBE.teams as te
-// ON te.id = ma.away_team
-// where in_progress = 0
-// GROUP BY te.team_name)) AS leaderboard
-// GROUP BY name) AS newBoard
-// ORDER BY totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC, goalsOwn DESC;
-
-// `;
-
 class LeaderboardService implements ILeaderboardService {
   constructor(private modelSequelize = sequelizeModel) {}
 
